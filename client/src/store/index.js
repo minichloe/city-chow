798d27b5eacf20760bfe47068fb0189125abf187
Chloe Chong <chloechong.us@gmail.com>
2018-06-23 17:44:53 -0400

    Removed some imports from store/index

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import user from './user'
 import favourites from './restaurants'
 import restaurants from './nearby'
