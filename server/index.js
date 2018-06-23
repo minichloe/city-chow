8c1c7a61dcc9ac65864888b17d05e36ad59fc136
Chloe Chong <chloechong.us@gmail.com>
2018-06-23 17:52:02 -0400

    Added index.html file

--- server/index.js
+++ server/index.js
@@ -47,6 +47,10 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || 'Internal server error.')
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, async () => {
